41b3582b6bf612a8c18b387f740745d9a4843e58
Fahim Abrar <132096309+r00kieBlinder@users.noreply.github.com>
2024-03-16 15:22:39 +0600

    initial commit

---
+++ function.py
@@ -0,0 +1,35 @@
+import numpy as np 
+
+# Numpy Universal Function
+np1 = np.array([-3,-2,-1,0,1,2,3,4,5,6,7,8,9])
+print(np1)
+
+
+# Square root of each element
+print(np.sqrt(np1))
+
+
+# Absolute Value 
+print(np.absolute(np1))
+
+
+# Exponents 
+print(np.exp(np1))
+ 
+
+# Min/Max
+print(np.min(np1))
+print(np.max(np1))
+
+
+# Sign positive/negative
+print(np.sign(np1))
+
+
+# Trig sin cos log
+print(np.sin(np1))
+print(np.cos(np1))
+print(np.log(np1))
+
+
+# and for a many more go to numpy.org functions documentation
